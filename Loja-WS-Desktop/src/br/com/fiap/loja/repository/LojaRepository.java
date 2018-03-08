0d6010f5a3eb7ebdd12b80e669bf3b1fd5c2f132
Daniel Lima <familiasilva.daniel@gmail.com>
2018-03-07 23:00:59 -0300

    Feito a implementação da LojaRepository

--- Loja-WS-Desktop/src/br/com/fiap/loja/repository/LojaRepository.java
+++ Loja-WS-Desktop/src/br/com/fiap/loja/repository/LojaRepository.java
@@ -1,8 +1,12 @@
 package br.com.fiap.loja.repository;
 
+import java.rmi.RemoteException;
+
 import org.apache.axis2.AxisFault;
 
 import com.fiap.loja.EstoqueBOStub;
+import com.fiap.loja.EstoqueBOStub.ConsultarProduto;
+import com.fiap.loja.EstoqueBOStub.ConsultarProdutoResponse;
 import com.fiap.loja.EstoqueBOStub.ProdutoTO;
 
 public class LojaRepository {
@@ -13,9 +17,14 @@ public class LojaRepository {
 		this.stub = new EstoqueBOStub();
 	}
 	
-	public ProdutoTO buscarProduto(int codigo) {
+	public ProdutoTO buscarProduto(int codigo) throws RemoteException {
 		
+		ConsultarProduto consulta = new ConsultarProduto();	
+		consulta.setCodigo(codigo);	
+		ConsultarProdutoResponse resp = stub.consultarProduto(consulta);
 		
+		ProdutoTO to = resp.get_return();		
+		return to;
 		
 	}
 
