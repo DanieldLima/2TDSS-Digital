eee91d0e60e66de89c14e0f4e83b6209c3c0fefa
logonrm <logonrm@fiap.com.br>
2018-04-04 21:00:41 -0300

    Revisão Restful

---
+++ 09-WS-Restful-Health/src/br/com/fiap/entity/Paciente.java
@@ -0,0 +1,66 @@
+package br.com.fiap.entity;
+
+import java.util.Calendar;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+@Table(name = "T_PACIENTE")
+@SequenceGenerator(name = "paciente", sequenceName = "SQ_T_PACIENTE", allocationSize = 1)
+public class Paciente {
+
+	@Id
+	@Column(name = "id_paciente")
+	@GeneratedValue(generator = "paciente", strategy = GenerationType.SEQUENCE)
+	private int codigo;
+
+	@Column(name = "nm_nome")
+	private String nome;
+
+	@Column(name = "dt_nascimento")
+	@Temporal(TemporalType.DATE)
+	private Calendar dataNascimento;
+
+	private boolean doador;
+
+	public int getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Calendar getDataNascimento() {
+		return dataNascimento;
+	}
+
+	public void setDataNascimento(Calendar dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+
+	public boolean isDoador() {
+		return doador;
+	}
+
+	public void setDoador(boolean doador) {
+		this.doador = doador;
+	}
+
+}
